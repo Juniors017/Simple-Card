05b05a6ae34d886ea7d524a02d62517a2d4af3ec
Juniors017 <Juniors017@users.noreply.github.com>
2025-06-10 20:57:24 +0000

    deploy: c6fbbdc0886eb155f07a3e53e78ba0b8b487e09f

---
+++ assets/js/acecf23e.842c988d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/Simple-Card/blog","blogTitle":"Blog","authorsListPath":"/Simple-Card/blog/authors"}')}}]);
